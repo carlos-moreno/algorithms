fb49924033aa87c44856160cc07dfa977fda908d
Carlos Moreno <omorenodovale@gmail.com>
2020-02-15 22:54:51 -0500

    Including tests for the best_stock method

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
-from algorithms import mult_two
+from algorithms import mult_two, best_stock
 
 class TestMultTwo(unittest.TestCase):
 
@@ -11,3 +11,14 @@ class TestMultTwo(unittest.TestCase):
 
     def test_mult_five_one_is_five(self):
         self.assertEqual(mult_two(5, 1), 5)
+
+class TestBestStock(unittest.TestCase):
+
+    def test_result_is_atx(self):
+        self.assertEqual(best_stock({"CAC": 10.0, "ATX": 390.2, "WIG": 1.2}), "ATX")
+    
+    def test_result_is_tasi(self):
+        self.assertEqual(best_stock({"CAC": 91.1, "ATX": 1.01, "TASI": 120.9}), "TASI")
+
+    def test_result_is_cbds(self):
+        self.assertEqual(best_stock({"FKS": 203.43, "TGAV": 1002.45, "CBDS": 1002.46}), "CBDS")
