767f3a034011c5a9516c84c1f6ab33549fb217a6
Carlos Moreno <omorenodovale@gmail.com>
2020-02-15 22:32:54 -0500

    Adding algorithms file with the mult_two method and test file

---
+++ algorithms.py
@@ -0,0 +1,3 @@
+def mult_two(a: int, b: int) -> int:
+    """Return multiply of two numbers"""
+    return a * b
---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+from algorithms import mult_two
+
+class TestMultiply(unittest.TestCase):
+
+    def test_mult_trhee_two_is_six(self):
+        self.assertEqual(mult_two(3, 2), 6)
+
+    def test_mult_one_zero_is_zero(self):
+        self.assertEqual(mult_two(1, 0), 0)
+
+    def test_mult_five_one_is_five(self):
+        self.assertEqual(mult_two(5, 1), 5)
