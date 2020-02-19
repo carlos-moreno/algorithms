a8643708d7ca9de8debc0326e3c0cdd391e0841e
Carlos Moreno <omorenodovale@gmail.com>
2020-02-18 22:08:28 -0500

    Adding method radix in algorithm file and your respective tests

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,10 @@
+import string
+
+
 def mult_two(a: int, b: int) -> int:
     """Return multiply of two numbers"""
     return a * b
+
 
 def best_stock(a: dict) -> str:
     """
@@ -8,5 +12,16 @@ def best_stock(a: dict) -> str:
     Input: The dictionary where the market identifier code is a key and the value is a stock price.
     Output: The market identifier code (ticker symbol) as a string.
     """
-    new_stock = sorted(a.items(), key = lambda d: (d[1], d[0]), reverse = True)
+    new_stock = sorted(a.items(), key=lambda d: (d[1], d[0]), reverse=True)
     return new_stock[0][0]
+
+
+def radix(str_number, radix):
+    digits = list(string.digits) + list(string.ascii_uppercase)
+    summation = 0
+    str_number = str_number[::-1]
+    for i, x in enumerate(str_number):
+        if digits.index(x) >= radix:
+            return -1
+        summation += digits.index(x) * pow(radix, i)
+    return summation
--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import unittest
-from algorithms import mult_two, best_stock
+from algorithms import mult_two, best_stock, radix
+
 
 class TestMultTwo(unittest.TestCase):
-
     def test_mult_trhee_two_is_six(self):
         self.assertEqual(mult_two(3, 2), 6)
 
@@ -12,13 +12,30 @@ class TestMultTwo(unittest.TestCase):
     def test_mult_five_one_is_five(self):
         self.assertEqual(mult_two(5, 1), 5)
 
-class TestBestStock(unittest.TestCase):
 
+class TestBestStock(unittest.TestCase):
     def test_result_is_atx(self):
         self.assertEqual(best_stock({"CAC": 10.0, "ATX": 390.2, "WIG": 1.2}), "ATX")
-    
+
     def test_result_is_tasi(self):
         self.assertEqual(best_stock({"CAC": 91.1, "ATX": 1.01, "TASI": 120.9}), "TASI")
 
     def test_result_is_cbds(self):
-        self.assertEqual(best_stock({"FKS": 203.43, "TGAV": 1002.45, "CBDS": 1002.46}), "CBDS")
+        self.assertEqual(
+            best_stock({"FKS": 203.43, "TGAV": 1002.45, "CBDS": 1002.46}), "CBDS"
+        )
+
+
+class TestRadix(unittest.TestCase):
+    def test_result_is_175(self):
+        self.assertEqual(radix("AF", 16), 175)
+
+    def test_result_is_5(self):
+        self.assertEqual(radix("101", 2), 5)
+
+    def test_result_is_26(self):
+        self.assertEqual(radix("101", 5), 26)
+
+    def test_result_is_1_negative(self):
+        self.assertEqual(radix("AB", 10), -1)
+        self.assertEqual(radix("909", 9), -1)
