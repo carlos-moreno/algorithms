b66f1052f294ab431b257abf3ad94295f0c67fea
Carlos Moreno <omorenodovale@gmail.com>
2020-02-20 10:29:10 -0500

    Including tests for the bigger_price method.

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from algorithms import (
     second_index,
     first_word,
     first_word_complex,
+    bigger_price,
 )
 
 
@@ -93,3 +94,27 @@ class TestFirstWorldComplex(unittest.TestCase):
 
     def test_result_is_and(self):
         self.assertEqual(first_word_complex("... and so on ..."), "and")
+
+
+class TestBiggerPrice(unittest.TestCase):
+    def test_result_is_2_dict(self):
+        self.assertEqual(
+            bigger_price(
+                2,
+                [
+                    {"name": "bread", "price": 100},
+                    {"name": "wine", "price": 138},
+                    {"name": "meat", "price": 15},
+                    {"name": "water", "price": 1},
+                ],
+            ),
+            [{"name": "wine", "price": 138}, {"name": "bread", "price": 100}],
+        )
+
+    def test_result_is_1_dict(self):
+        self.assertEqual(
+            bigger_price(
+                1, [{"name": "pen", "price": 5}, {"name": "whiteboard", "price": 170}]
+            ),
+            [{"name": "whiteboard", "price": 170}],
+        )
