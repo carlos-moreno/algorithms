c0fdfc7e18d175134fb7e29a22371b8c85ed1c53
Carlos Moreno <omorenodovale@gmail.com>
2020-02-22 22:41:02 -0500

    Adding method frequency_sort in algorithm file.

--- algorithms.py
+++ algorithms.py
@@ -171,23 +171,3 @@ def frequency_sort(items):
         return sorted(
             sorted(items, reverse=True), key=lambda k: (items.count(k)), reverse=True
         )
-
-
-if __name__ == "__main__":
-    print("Example:")
-    print(frequency_sort([4, 6, 2, 2, 6, 4, 4, 4]))
-
-    # These "asserts" are used for self-checking and not for an auto-testing
-    assert list(frequency_sort([4, 6, 2, 2, 6, 4, 4, 4])) == [4, 4, 4, 4, 6, 6, 2, 2]
-    assert list(frequency_sort(["bob", "bob", "carl", "alex", "bob"])) == [
-        "bob",
-        "bob",
-        "bob",
-        "carl",
-        "alex",
-    ]
-    assert list(frequency_sort([17, 99, 42])) == [17, 99, 42]
-    assert list(frequency_sort([])) == []
-    assert list(frequency_sort([1])) == [1]
-    assert list(frequency_sort([1, 2, 2, 1])) == [1, 1, 2, 2]
-    print("Coding complete? Click 'Check' to earn cool rewards!")
