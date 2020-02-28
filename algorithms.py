ff8a6e502433cbc1d3233c3676975ea344657ba7
Carlos Moreno <omorenodovale@gmail.com>
2020-02-27 19:47:36 -0500

    Adding method worth_of_words in algorithm file.

--- algorithms.py
+++ algorithms.py
@@ -234,3 +234,23 @@ def translate(phrase):
             index += 2
 
     return "".join(result)
+
+
+VALUES = {'e': 1,  'a': 1, 'i': 1, 'o': 1, 'n': 1, 'r': 1,
+          't': 1,  'l': 1, 's': 1, 'u': 1, 'd': 2, 'g': 2,
+          'b': 3,  'c': 3, 'm': 3, 'p': 3, 'f': 4, 'h': 4,
+          'v': 4,  'w': 4, 'y': 4, 'k': 5, 'j': 8, 'x': 8,
+          'q': 10, 'z': 10}
+
+
+def worth_of_words(words):
+    """
+        Return the most valuable word.
+    """
+    result = {}
+    for v in words:
+        _sum = 0
+        for it in v:
+            _sum += VALUES.get(it)
+        result[v] = _sum
+    return sorted(result.items(), key=lambda k: (k[1]), reverse=True)[0][0]
