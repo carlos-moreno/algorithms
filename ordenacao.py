798e4ba328432628d130f5f844d7cd33175df489
Carlos Moreno <omorenodovale@gmail.com>
2020-04-10 19:53:57 -0500

    Update in file ordenacao.py

--- ordenacao.py
+++ ordenacao.py
@@ -1,24 +1,78 @@
-class Ordenacao:
-    def __init__(self, lista):
-        self.lista = lista
+from random import randrange
+from time import time
 
-    def bubblesort(self):
+
+class Ordenacao:
+    def __init__(self, size):
+        self.size = size
+
+    def bubblesort(self, lst):
         """
             Must return list sort using the bubble sort.
         """
-        fim = len(self.lista)
-        for i in range(fim - 1, 0, -1):
+        end = len(lst)
+        for i in range(end - 1, 0, -1):
             for j in range(i):
-                if self.lista[j] > self.lista[j + 1]:
-                    self.lista[j], self.lista[j + 1] = self.lista[j + 1], self.lista[j]
+                if lst[j] > lst[j + 1]:
+                    lst[j], lst[j + 1] = lst[j + 1], lst[j]
 
-        return self.lista
+        return lst
+
+    def bubblesort_v2(self, lst):
+        """
+            Must return list sort using the bubble sort.
+        """
+        end = len(lst)
+        for i in range(end - 1, 0, -1):
+            change = False
+            for j in range(i):
+                if lst[j] > lst[j + 1]:
+                    lst[j], lst[j + 1] = lst[j + 1], lst[j]
+                    change = True
+            if not change:
+                return lst
+
+        return lst
+
+    def direct_selection(self, lst):
+        end = len(lst)
+
+        for i in range(end - 1):
+            min_position = i
+            for j in range(i + 1, end):
+                if lst[j] < lst[min_position]:
+                    min_position = j
+            lst[i], lst[min_position] = lst[min_position], lst[i]
+
+        return lst
+
+    def list_generation(self):
+        lst = [randrange(-10000, 10000) for _ in range(self.size)]
+        return lst
+
+    def list_generation_v2(self):
+        lst = [x for x in range(self.size)]
+        lst[self.size // 10] = randrange(len(lst))
+        return lst
 
 
 if __name__ == "__main__":
-    o1 = Ordenacao([2, 3, 10, -9, 50, 1])
-    print(o1.bubblesort())
-    o2 = Ordenacao([-13, 2, 100, -9230, 23, 10])
-    print(o2.bubblesort())
-    o3 = Ordenacao([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
-    print(o3.bubblesort())
+    o = Ordenacao(5000)
+    lst_1 = o.list_generation_v2()
+    lst_2 = lst_1.copy()
+    lst_3 = lst_1.copy()
+
+    before = time()
+    o.bubblesort(lst_1)
+    after = time()
+    print(f"The execution time bubblesort is {after - before}")
+
+    before = time()
+    o.bubblesort_v2(lst_2)
+    after = time()
+    print(f"The execution time bubblesort v2 is {after - before}")
+
+    before = time()
+    o.direct_selection(lst_3)
+    after = time()
+    print(f"The execution time direct_selection is {after - before}")
