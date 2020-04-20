4d39a6d3034df802e74f1b60c79e2c2e6c2a3121
Carlos Moreno <omorenodovale@gmail.com>
2020-04-20 10:57:57 -0500

    Add methods merge_sort and merge

--- ordenacao.py
+++ ordenacao.py
@@ -63,3 +63,28 @@ class Ordenacao:
         lst = [x for x in range(self.size)]
         lst[self.size // 10] = randrange(len(lst))
         return lst
+
+
+def merge_sort(lista):
+    if len(lista) <= 1:
+        return lista
+
+    meio = len(lista) // 2
+
+    lado_esquerdo = merge_sort(lista[:meio])
+    lado_direito = merge_sort(lista[meio:])
+
+    return merge(lado_esquerdo, lado_direito)
+
+
+def merge(lado_esquerdo, lado_direito):
+    if not lado_esquerdo:
+        return lado_direito
+
+    if not lado_direito:
+        return lado_esquerdo
+
+    if lado_esquerdo[0] < lado_direito[0]:
+        return [lado_esquerdo[0]] + merge(lado_esquerdo[1:], lado_direito)
+
+    return [lado_direito[0]] + merge(lado_esquerdo, lado_direito[1:])
