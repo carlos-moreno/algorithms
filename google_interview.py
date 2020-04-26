e04f1e9c7459a47d22e2e232d7f181e78505d0c5
Carlos Moreno <omorenodovale@gmail.com>
2020-04-25 23:53:36 -0500

    Add question google interview

---
+++ google_interview.py
@@ -0,0 +1,54 @@
+"""
+Give a number N, write a code to print all positive numbers less than N in
+which all adjacent digits differ by 1
+
+>>> number_adjacent(105)
+0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 21, 23, 32, 34, 43, 45, 54, 56, 65, 67, 76, 78, 87, 89, 98, 101
+
+>>> number_adjacent(30)
+0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 21, 23
+"""
+
+
+def len_(number):
+    return len(str(number))
+
+
+def first_number_to_sum(number):
+    if len_(number) > 1:
+        return int(str(number)[-2])
+
+
+def second_number_to_sum(number):
+    if len_(number) > 1:
+        return int(str(number)[-1])
+
+
+def smallest_to_largest(number):
+    return first_number_to_sum(number) - second_number_to_sum(number)
+
+
+def largest_to_smallest(number):
+    return second_number_to_sum(number) - first_number_to_sum(number)
+
+
+def number_is_valid(number):
+    return (
+        len_(number) == 1
+        or smallest_to_largest(number) == 1
+        or largest_to_smallest(number) == 1
+    )
+
+
+def number_adjacent(number: int) -> str:
+    l = []
+    for n in range(number):
+        if number_is_valid(n):
+            l.append(n)
+    print(", ".join(str(n) for n in l))
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
