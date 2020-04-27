40deafc461a7a43c991cda1d71be59e46b904174
Carlos Moreno <omorenodovale@gmail.com>
2020-04-27 00:21:24 -0500

    Refactoring method build_print

--- print_borders.py
+++ print_borders.py
@@ -1,13 +1,16 @@
-def build_print(number):
-    n = 1
-    while n < number:
-        if n < 3:
-            print(f'{"1" * n}')
-        else:
-            print(f'{1}{" " * (n - 2)}{"1"}')
-        n += 1
-    print(f'{"1" * number}')
+def build_print(number, n):
+    if n < 3:
+        s = f'{"1" * n}'
+    elif n > 2 and n <  number:
+        s = f'{"1"}{" " * (n - 2)}{"1"}'
+    else:
+        s = f'{"1" * number}'
+    return s
+
+def print_(number):
+    for n in range(1, number + 1):
+        print(build_print(number, n))
 
 
 if __name__ == "__main__":
-    build_print(5)
+    print_(5)
