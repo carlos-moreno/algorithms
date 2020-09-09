79d0d1ceed6d4c86ac4c03b4dc0326a69d96ffa3
Carlos Moreno <omorenodovale@gmail.com>
2020-09-09 08:19:41 -0300

    Removing unnecessary lines

--- binary.py
+++ binary.py
@@ -1,19 +1,3 @@
-def binary(num, l=[]):
-    """Function to return a number decimal in binary
-
-    >>> binary(20)
-    '00010100'
-    >>> binary(75)
-    '01001011'
-
-    :param num: number decimal
-    """
-    while num > 0:
-        l.append(str(num % 2))
-        num //= 2
-    return ''.join(l[::-1]).zfill(8)
-
-
 class Converter:
     def __init__(self, num):
         self.num = num
